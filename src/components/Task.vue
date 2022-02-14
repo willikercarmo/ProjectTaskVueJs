9f2901bd03fe81e3a6f284cdeaf3923c3c4b6169
Williker Carmo <carmo.williker@gmail.com>
2022-02-14 15:30:26 +0000

    Added task list

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -6,24 +6,32 @@
       <div class="col-lg-8 offset-lg-2">
         <div class="card">
           <div class="card-body">
-              <div class="input-group">
-                  <input type="text" class="form-control form-control-lg" placeholder="Add tak">
-                  <div class="input-group-append">
-                      <button class="btn btn-success btn-lg">Add</button>
-                  </div>
+            <div class="input-group">
+              <input
+                type="text"
+                class="form-control form-control-lg"
+                placeholder="Add tak"
+                v-model="task"
+              />
+              <div class="input-group-append">
+                <button @click="addTask()"
+                class="btn btn-success btn-lg">Add</button>
               </div>
-              <br>
-              <ul class="list-group">
-                  <li class="list-group-item d-flex justify-content-between">
-                      <span class="cursor">
-                          <i class="far fa-circle"></i>
-                      </span>
-                      To do the task
-                      <span class="text-danger cursor">
-                          <i class="fas fa-trash-alt"></i>
-                      </span>
-                  </li>
-              </ul>
+            </div>
+            <br />
+            
+            <ul class="list-group">
+              <li v-for="(task, index) of listTasks" :key="index"
+              class="list-group-item d-flex justify-content-between">
+                <span class="cursor">
+                  <i class="far fa-circle"></i>
+                </span>
+                {{task.name}}
+                <span class="text-danger cursor">
+                  <i class="fas fa-trash-alt"></i>
+                </span>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
@@ -34,11 +42,27 @@
 <script>
 export default {
   name: "Task",
+  data() {
+      return {
+          task: '',
+          listTasks: []
+      }
+  },
+  methods: {
+      addTask(){
+          const task = {
+              name: this.task,
+              status: false
+          }
+          this.listTasks.push(task);
+          this.task = '';
+      }
+  },
 };
 </script>
 
 <style scoped>
-.cursor{
-    cursor: pointer;
+.cursor {
+  cursor: pointer;
 }
 </style>
