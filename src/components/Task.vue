ff4f51fbbcfee3c218d69312a08c35988d213c8b
Williker Carmo <carmo.williker@gmail.com>
2022-02-14 14:32:19 +0000

    TaskComponent HTML

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
-    <h1>Hello World!</h1>
+    <h1 class="display-4 text-center">To-Do List</h1>
+    <hr />
+    <div class="row">
+      <div class="col-lg-8 offset-lg-2">
+        <div class="card">
+          <div class="card-body">
+              <div class="input-group">
+                  <input type="text" class="form-control form-control-lg" placeholder="Add tak">
+                  <div class="input-group-append">
+                      <button class="btn btn-success btn-lg">Add</button>
+                  </div>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
